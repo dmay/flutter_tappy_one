f34ebc75af4b05783894425957ec160626ffc2da
Dmitriy Maygurov <dmitriy.maygurov@gmail.com>
2019-04-01 22:37:40 -0700

    Runnable

---
+++ lib/MainLoop.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class MainLoop extends Game{
+  
+  @override
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  @override
+  void update(double t) {
+    // TODO: implement update
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:tappy_one/MainLoop.dart';
 
-void main() {}
+void  main() async {
+  var flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  var mainLoop = MainLoop();
+
+  runApp(mainLoop.widget);
+}
 
